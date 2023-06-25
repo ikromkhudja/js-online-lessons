ed5b8784efccfe96de1c2b38b480756286379559
ikromkhuja <tashpulatovikromkhuja@gmail.com>
2023-06-25 20:24:48 +0500

    ozgaruvchilar va malumot turlari

---
+++ main.js
@@ -0,0 +1,48 @@
+//1-ma'lumot turi number
+//+/- 2\53 -1
+
+let age = 1.1
+let hour = 2
+console.log(age+hour)
+//2-ma'lumot turi - BigInt
+//100n
+
+
+//3-ma'lumot turi 
+let name = 'ikormxoja'
+let name2 = "ikorm"
+let name3 = `qalesan ${name2}`
+
+console.log(name3)
+
+//4-ma'lumot turi - boolean    true and false
+let number = true
+
+//5-ma'lumot turi - object
+//object-da kalit faqat string | number |symbol  boladi 
+let custom = {
+    ism: 'ikrom',
+    yosh: 19,
+}
+
+//6-ma'lumot turi 
+//Symbol
+
+let key = Symbol('kalit')
+
+// Symbol('kalit') === Symbol('kalit') =  false 
+
+//7-ma'lumot turi 
+//Null -> hech narsa yoq | bom bosh
+
+let user = null;
+
+//8-ma'lumot turi
+//undefined ->biriktirilmagan
+
+let variables = undefined
+
+console.log(typeof variables)
+
+//constants
+const limit =12
